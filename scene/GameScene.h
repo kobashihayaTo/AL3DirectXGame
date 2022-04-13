304b3676bd116d8c97a9eb021f06072721eb5c00
kobashi_hayato <k021g1128@g.neec.ac.jp>
2022-04-13 20:13:33 +0900

    01_02_エンジンの使い方

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -16,6 +16,14 @@
 /// </summary>
 class GameScene {
 
+	//スプライト
+	Sprite* sprite_ = nullptr;
+	//3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロダクション
+	ViewProjection viewProjection_;
   public: // メンバ関数
 	/// <summary>
 	/// コンストクラタ
@@ -48,6 +56,14 @@ class GameScene {
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
 
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	//サウンドデータバンドル
+	uint32_t soundDateHandle_ = 0;
+	//音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+	//値を表示したい変数
+	int32_t value_ = 0;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
