e9db897524763d233498d71edb58eb6e806d361d
kobashi_hayato <k021g1128@g.neec.ac.jp>
2022-04-17 21:08:10 +0900

    02_03_射影変換行列

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -24,7 +24,7 @@ void GameScene::Initialize() {
 	sprite_ = Sprite::Create(textureHandle_, {100, 50});
 	// 3Dモデルの生成
 	model_ = Model::Create();
-	
+
 	//乱数シード生成器
 	std::random_device seed_gen;
 	//メルセンヌ・ツイスター
@@ -33,102 +33,139 @@ void GameScene::Initialize() {
 	std::uniform_real_distribution<float> rotDist(0.0f, XM_2PI);
 	//乱数範囲（座標用）
 	std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
-	
+
 	for (size_t i = 0; i < _countof(worldTransform_); i++) {
 		// X,Y,Z方向のスケーリングを設定
 		worldTransform_[i].scale_ = {1.0f, 1.0f, 1.0f};
 		// X,Y,Z軸周りの回転角を設定
-		worldTransform_[i].rotation_ = {rotDist(engine), rotDist(engine),rotDist(engine)};
+		worldTransform_[i].rotation_ = {rotDist(engine), rotDist(engine), rotDist(engine)};
 		// X,Y,Z軸周りの平行移動を設定
 		worldTransform_[i].translation_ = {posDist(engine), posDist(engine), posDist(engine)};
 		//ワールドトランスフォームの初期化
 		worldTransform_[i].Initialize();
 	}
-	////カメラ視点座標を設定
-	//viewProjection_.eye = {0, 0, -10};
-	////カメラ注視点座標を設定
-	//viewProjection_.target = {10, 0, 0};
-	//カメラ上方向ベクトルを設定(右上45度指定)
-	viewProjection_.up = {cosf(XM_PI / 4.0f), sinf(XM_PI / 4.0f), 0.0f};
+
+	////カメラ垂直方向視野角を設定
+	// viewProjection_.fovAngleY = XMConvertToRadians(10.0f);
+
+	////アスペクト比を設定
+	// viewProjection_.aspectRatio = 1.0f;
+
+	//ニアクリップ距離を設定
+	viewProjection_.nearZ = 52.0f;
+	//ファークリップ距離を設定
+	viewProjection_.farZ = 53.0f;
+
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
-
-	
 }
 
-void GameScene::Update() { 
+void GameScene::Update() {
 	//視点移動処理
-	{
-		//視点の移動ベクトル
-		XMFLOAT3 move = {0, 0, 0};
-		//視点の移動速さ
-		const float KEyeSpeed = 0.2f; 
-		//押した方向で移動ベクトルを変更
-		if (input_->PushKey(DIK_W)) {
-			move = {0, 0, KEyeSpeed};
-		} else if (input_->PushKey(DIK_S)) {
-			move = {0, 0, -KEyeSpeed};
-		}
-		//視点移動(ベクトルの加算)
-		viewProjection_.eye.x += move.x;
-		viewProjection_.eye.y += move.y;
-		viewProjection_.eye.z += move.z;
-		//行列の再計算
-		viewProjection_.UpdateMatrix();
-		//デバック用表示
-		debugText_->SetPos(50, 50);
-		debugText_->Printf(
-		  "eye:(%f,%f,%f)",
-			viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
-	//注視点移動処理
-		{
-			//注視点移動座標
-			XMFLOAT3 move = {0, 0, 0};
-			//注視点の移動の速さ
-			const float kTaragetSpeed = 0.2f;
-			//押した方向で移動ベクトルを変更
-			if (input_->PushKey(DIK_LEFT)) {
-				move = {-kTaragetSpeed, 0, 0};
-			} else if (input_->PushKey(DIK_RIGHT)) {
-				move = {kTaragetSpeed, 0, 0};
-			}
-			//注視点移動(ベクトルの加算)
-			viewProjection_.target.x += move.x;
-			viewProjection_.target.y += move.y;
-			viewProjection_.target.z += move.z;
+	{////視点の移動ベクトル
+	 // XMFLOAT3 move = {0, 0, 0};
+	 ////視点の移動速さ
+	 // const float KEyeSpeed = 0.2f;
+	 ////押した方向で移動ベクトルを変更
+	 // if (input_->PushKey(DIK_W)) {
+	 //	move = {0, 0, KEyeSpeed};
+	 // } else if (input_->PushKey(DIK_S)) {
+	 //	move = {0, 0, -KEyeSpeed};
+	 // }
+	 ////視点移動(ベクトルの加算)
+	 // viewProjection_.eye.x += move.x;
+	 // viewProjection_.eye.y += move.y;
+	 // viewProjection_.eye.z += move.z;
+	 ////行列の再計算
+	 // viewProjection_.UpdateMatrix();
+	 ////デバック用表示
+	 // debugText_->SetPos(50, 50);
+	 // debugText_->Printf(
+	 //   "eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
 
-			//行列の再利用
-			viewProjection_.UpdateMatrix();
-			//デバック用表示
-			debugText_->SetPos(50, 70);
-			debugText_->Printf("target:(%f,%f,%f)",viewProjection_.target.x,
-				viewProjection_.target.y,viewProjection_.target.z);
-		}
+	 // FoV変更処理
+	 {//上キーで視野角が広がる
+	  if (input_->PushKey(DIK_UP)){viewProjection_.fovAngleY += 0.01f;
+	viewProjection_.fovAngleY = min(viewProjection_.fovAngleY, XM_PI);
+	//下キーで視野角が狭まる
+}
+else if (input_->PushKey(DIK_DOWN)) {
+	viewProjection_.fovAngleY -= 0.01f;
+	viewProjection_.fovAngleY = max(viewProjection_.fovAngleY, 0.01f);
+}
+
+//行列の再計算
+viewProjection_.UpdateMatrix();
+
+//デバック用表示
+debugText_->SetPos(50, 110);
+debugText_->Printf("fovAngleY(Degree):%f", XMConvertToDegrees(viewProjection_.fovAngleY));
+}
+
+//クリップ距離変更処理
+{
+	//上下キーでニアクリップ距離を増減
+	if (input_->PushKey(DIK_UP)) {
+		viewProjection_.nearZ += 0.1f;
+	} else if (input_->PushKey(DIK_DOWN)) {
+		viewProjection_.nearZ -= 0.1f;
 	}
-	//上方向回転処理
-	{
-		//上の回転速さ[ラジアン/frame]
-		const float kUpRotSpeed = 0.05f;
-		//押した方向で移動ベクトルを変更
-		if (input_->PushKey(DIK_SPACE)) {
-			viewAngle += kUpRotSpeed;
-			//2π越えたら0に戻す
-			viewAngle = fmodf(viewAngle, XM_2PI);
-		}
-		//上方向ベクトルを計算
-		viewProjection_.up = {cosf(viewAngle), sinf(viewAngle), 0.0f};
-		//行列の再計算
-		viewProjection_.UpdateMatrix();
-		//デバック再表示
-		debugText_->SetPos(50, 50);
-		debugText_->Printf(
-		  "up:(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y,
-			viewProjection_.up.z);
+	//行列の再計算
+	viewProjection_.UpdateMatrix();
+
+	//デバック用表示
+	debugText_->SetPos(50, 130);
+	debugText_->Printf("nearZ:%f", viewProjection_.nearZ);
+}
+//注視点移動処理
+{
+	//注視点移動座標
+	XMFLOAT3 move = {0, 0, 0};
+	//注視点の移動の速さ
+	const float kTaragetSpeed = 0.2f;
+	//押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_LEFT)) {
+		move = {-kTaragetSpeed, 0, 0};
+	} else if (input_->PushKey(DIK_RIGHT)) {
+		move = {kTaragetSpeed, 0, 0};
 	}
+	//注視点移動(ベクトルの加算)
+	viewProjection_.target.x += move.x;
+	viewProjection_.target.y += move.y;
+	viewProjection_.target.z += move.z;
+
+	//行列の再利用
+	viewProjection_.UpdateMatrix();
+	//デバック用表示
+	debugText_->SetPos(50, 70);
+	debugText_->Printf(
+	  "target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y,
+	  viewProjection_.target.z);
+}
+}
+//上方向回転処理
+{
+	//上の回転速さ[ラジアン/frame]
+	const float kUpRotSpeed = 0.05f;
+	//押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_SPACE)) {
+		viewAngle += kUpRotSpeed;
+		// 2π越えたら0に戻す
+		viewAngle = fmodf(viewAngle, XM_2PI);
+	}
+	//上方向ベクトルを計算
+	viewProjection_.up = {cosf(viewAngle), sinf(viewAngle), 0.0f};
+	//行列の再計算
+	viewProjection_.UpdateMatrix();
+	//デバック再表示
+	debugText_->SetPos(50, 90);
+	debugText_->Printf(
+	  "up:(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y, viewProjection_.up.z);
+}
 }
 
 void GameScene::Draw() {
-	
+
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
